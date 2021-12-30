6a46eba2dfdc6bbb3d8785d33a72919da35b737b
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2021-12-30 23:35:10 +0300

    Update example1.cpp

--- ders_22/example1.cpp
+++ ders_22/example1.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <optional>
+#include <string>
 
+std::optional<int> to_int(const std::string& s)
+{
+  std::optional<int> ret;
+	try {
+		ret = std::stoi(s);
+	}
+	catch (...)
+	{
+	}
+	return ret;				
+}
+
+int main()
+{
+	for (auto str : { "345", "987","alican","01555" })
+	{
+		if (auto op = to_int(str)) {
+			std::cout << "gecerli deger : " << *op << "\n";
+		}
+		else
+		{
+			std::cout << "gecersiz tamsayi "<< str <<"\n";
+		}
+	}
+}
