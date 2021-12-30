41c6a8084633046555b6f55d3222ec87f0872b9e
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2021-12-31 00:18:38 +0300

    Create example11.cpp

---
+++ ders_22/example11.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<variant>
+
+class Data {
+public:
+	Data(int x) : mx{ x } {}
+private:
+	int mx;
+};
+
+int main()
+{
+	using namespace std;
+
+	variant<monostate, int, double, Data> vx; // valid
+	std::cout << "index = " << vx.index() << '\n'; // 0
+
+	variant<Data, int> va; //Error
+	
+	variant<monostate, Data, int, double> vy;
+	std::cout << "index = " << vy.index() << '\n'; // 0
+	
+	if (holds_alternative<monostate>(vy)) 
+	{
+		std::cout << "Variant bos\n";
+	}
+}
