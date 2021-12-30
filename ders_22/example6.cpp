63d0d7b478a63d3d57ed66400f4868ff04fce523
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2021-12-30 23:56:57 +0300

    Update example6.cpp

--- ders_22/example6.cpp
+++ ders_22/example6.cpp
@@ -3,29 +3,15 @@
 #include<string>
 #include"Date.h"
 
-class Date
-{
-public:
-	Date(int mx, int my, int mz)
-	{
-
-	}
-	
-	Date()
-	{
-
-	}
-};
-
 int main()
 {
 	using namespace std;
 
 	variant<Date, int, double, string> vx; // valid
-	vx = "Ali";							   // valid
-	get<0>(vx) = Date{ 1,5,1987 };		   // valid
+	vx = "Ali";			       // valid
+	get<0>(vx) = Date{ 1,5,1987 };	       // valid
 	get<Date>(vx) = Date{ 1,5,1987 };      // valid
 	cout << get<Date>(vx) << "\n";         // valid
-	cout << get<float>(vx);				   // Error.Float doesn't exist
-	cout << get<4>(vx);					   // Error.Index error
+	cout << get<float>(vx);		       // Error.Float doesn't exist
+	cout << get<4>(vx);		       // Error.Index error
 }
