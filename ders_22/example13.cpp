e7823408a65933ba08d964f0396e33208a1a4bdd
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-02 11:57:03 +0300

    Create example13.cpp

---
+++ ders_22/example13.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <variant>
+
+int main()
+{
+	using namespace std;
+
+	variant<int, double, long> vx{ 1 };
+	std::cout << vx.index() << '\n'; // 0
+
+	vx = 2.3;
+	std::cout << vx.index() << '\n'; // 1
+
+	vx = 5L;
+	std::cout << vx.index() << '\n'; // 2
+
+}
