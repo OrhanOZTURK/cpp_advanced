c741f26251d33e4102c700d82d31609af08bbb8e
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-02 16:37:46 +0300

    Update example3.cpp

--- ders_25/example3.cpp
+++ ders_25/example3.cpp
@@ -14,10 +14,10 @@ int main()
 	Point a;
 	a.x = 3.4;
 		
-	Point p = { .x = 3, .y = 5, .z = 5 }; 
+	Point p = { .x = 3, .y = 5, .z = 5 };  //valid
 	std::vector<Point> pvec;
 
-	pvec.push_back(Point{ .x = 1.1, .y = 2.2, .z = 3.3 }); 
+	pvec.push_back(Point{ .x = 1.1, .y = 2.2, .z = 3.3 });  //valid
 
-	pvec.push_back(Point{ .x = {1.1}, .y = {2.2}, .z = {3.3} }); 
+	pvec.push_back(Point{ .x = {1.1}, .y = {2.2}, .z = {3.3} });  //valid
 }
