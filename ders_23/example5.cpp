df5f219a40f7516b575f06156f31a47d4777eef6
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-02 12:27:10 +0300

    Create example5.cpp

---
+++ ders_23/example5.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+#include<variant>
+
+
+class Circle {
+public:
+	void render()
+	{
+		std::cout << "circle is being rendered\n";
+	}
+};
+class Square {
+public:
+	void render()
+	{
+		std::cout << "Square is being rendered\n";
+	}
+};
+class Triangle {
+public:
+	void render()
+	{
+		std::cout << "Triangle is being rendered\n";
+	}
+};
+class Polygon {
+public:
+	void render()
+	{
+		std::cout << "Polygon is being rendered\n";
+	}
+};
+
+using shape = std::variant<Circle, Square, Triangle, Polygon>;
+
+int main()
+{
+	using namespace std;
+
+	vector<shape> svec;
+	svec.emplace_back(Circle{});
+	svec.emplace_back(Square{});
+	svec.emplace_back(Triangle{});
+	svec.emplace_back(Polygon{});
+
+	
+	for (auto& object : svec)
+	{
+		visit([](auto& object) {object.render(); }, object);
+	}
+}
