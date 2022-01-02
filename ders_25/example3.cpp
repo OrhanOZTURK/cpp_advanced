2c7c6b316f560f33706a218676e48a5182a1aae2
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-02 19:37:34 +0300

    Create example3.cpp

---
+++ ders_25/example3.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <format>
+
+int main()
+{
+	using namespace std;
+
+	double dval = 1545687234.5213213214325678912;
+
+	cout << format("{}\n", dval); // 1545687234.5213213
+
+	cout << format("{:f}\n", dval); // 1545687234.521321
+
+	cout << format("{:E}\n", dval); // 1.545687E+09
+	
+	std::cout << format("{}\n", "necati ergin"); // necati ergin
+	
+	std::cout << format("{:s}\n", "necati ergin"); //  necati ergin
+
+	std::cout << format("{:s}\n", "10 > 5"); // 10 > 5
+	
+	//------------------------------------------------------------------------------------------------------
+
+	int x = 7894;
+	std::cout << format("|{:18}| {}", x, "ali") << "\n"; //  |              7894| ali
+
+	std::cout << format("|{:<18}| {}", x, "ali") << "\n"; // |7894              | ali
+	
+	std::cout << format("|{:>18}| {}", x, "ali") << "\n"; 	// |              7894| ali
+
+	std::cout << format("|{:^18}| |{}|", x, "ali") << "\n";  // |       7894       | |ali|
+
+	std::cout << format("|{:!<18}| {}", x, "ali") << "\n";    // |7894!!!!!!!!!!!!!!| ali
+
+	std::cout << format("|{:$>18}| {}", x, "ali") << "\n"; // |$$$$$$$$$$$$$$7894| ali
+
+	std::cout << format("|{:.^18}| |{}|", x, "ali") << "\n";  // |.......7894.......| |ali|
+
+	std::cout << format("|{:6}|", "NECATI ERGIN") << "\n"; // |NECATI ERGIN|
+
+	//------------------------------------------------------------------------------------------------------
+
+	int y = 6;
+
+	std::cout << format("|{:.{}}|", "NECATI ERGIN", y) << "\n"; // |NECATI|
+
+	//------------------------------------------------------------------------------------------------------
+	
+	int z = 762374;
+	std::cout << format("|{:15x}|", z) << "\n"; // |          ba206|
+	
+	std::cout << format("|{:15X}|", z) << "\n"; // |          BA206|
+	
+	std::cout << format("|{:<#15X}|", z) << "\n"; // |0XBA206        |
+	
+	std::cout << format("|{:_<#15x}|", z) << "\n"; // |0xba206________|
+
+}
