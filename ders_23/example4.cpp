32591c66e31eb0a180711b4f773a26057ed7f5f3
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-02 12:25:28 +0300

    Update example4.cpp

--- ders_23/example4.cpp
+++ ders_23/example4.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+#include<variant>
 
+
+int main()
+{
+	using namespace std;
+
+	variant<string, int, double, string> vx;
+
+	vx.emplace<0>("murat");		//valid
+	cout << get<0>(vx) << "\n"; //valid
+
+	std::cout << get<string>(vx) << "\n"; // Ambigiuty 
+
+	vx.emplace<3>("aksoy");			 //valid
+	std::cout << get<3>(vx) << "\n"; //valid
+}
