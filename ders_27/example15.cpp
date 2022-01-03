5e0284ce857b9939a0f14c83ad722575537bf9e7
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-03 15:52:45 +0300

    Update example15.cpp

--- ders_27/example15.cpp
+++ ders_27/example15.cpp
@@ -14,5 +14,5 @@ struct Nec {
 		return static_cast<T::Neco>(x); 
 	}
 
-	auto f()->Nec<T>::ptr; // Trailing return type. No need use typename.
+	auto f()->Nec<T>::ptr; // C++20. Trailing return type. No need use typename.
 };
