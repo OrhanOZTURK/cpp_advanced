afaa1d58b85634d136aa4aecceda018942ac5d9b
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-03 16:26:54 +0300

    Create example21.cpp

---
+++ ders_27/example21.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+struct Data {
+public:
+	Data(Data&&) noexcept(false);
+};
+
+class Myclass {
+public:
+	Myclass(Myclass&&) = default;
+private:
+	Data x;
+};
+
+int main()
+{
+	static_assert(std::is_nothrow_move_constructible_v<Myclass>); //Error
+}
