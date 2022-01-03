4b40c83075c620179c13275cb065da25cb570836
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-03 16:46:52 +0300

    Create example24.cpp

---
+++ ders_27/example24.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+struct Nec
+{
+	int x;
+	bool flag;
+};
+
+int main()
+{
+	void* vptr{};
+	Nec nec{ 12,vptr }; //Valid in C++17. Warning in C++20. void * to bool conversion is narrowing conversion.
+	
+	int x = 10;
+	bool b{ &x }; // Syntax Error in C++20. Narrowing conversion
+}
