9fa82d7baa09213d28e497f9ef66af9d14c922e1
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-03 13:42:04 +0300

    Create example7.cpp

---
+++ ders_27/example7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+struct Data{
+	int x,y;
+	struct Nested {
+		int a,b;
+	}z;
+};
+
+int main()
+{
+	struct Data mydata = { 1,2,{3,4} }; // Valid
+
+	struct Data mydata = { 1,2,3,4 }; // Valid
+
+	struct Data mydata = (1, 2, 3, 4); // Syntax Error
+}
