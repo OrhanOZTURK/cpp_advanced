c6f35ebf033b8523f7a29c74eb20dcc7b9d31a17
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-03 15:41:37 +0300

    Create example14.cpp

---
+++ ders_27/example14.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+template <typename T>
+struct PointerTrait {
+	using Ptr = void*;
+};
+template<typename T>
+struct Nec {
+	using Ptr = PointerTrait<T>::Ptr;
+
+	T::Neco foo(T::Ali x);
+	{
+		return static_cast<T::Neco>(x); //Valid
+	}
+};
