04b543c2eadf1bd66152a1c3fd43a836dabdfc3e
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-03 16:23:51 +0300

    Create example20.cpp

---
+++ ders_27/example20.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+class Myclass {
+public:
+	Myclass(Myclass&&) = default;
+};
+
+int main()
+{
+	static_assert(std::is_nothrow_move_constructible_v<Myclass>); // Ctor noexcept
+}
