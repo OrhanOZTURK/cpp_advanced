9690e052e00dda2071369087f4a1a11edb0d2518
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-03 16:35:46 +0300

    Update example23.cpp

--- ders_27/example23.cpp
+++ ders_27/example23.cpp
@@ -6,5 +6,17 @@ struct Nec {
 	
 	//explicit(true) Nec(T); // C++20. Constructor is explicit
 	
-	//explicit(std::is_integral_v<T>)Nec(T); // C++20
+	//explicit(std::is_integral_v<T>)Nec(T); // C++20. Conditionally
+};
+
+
+template<typename T>
+struct Myclass{
+	explicit(!std::is_convertible_v<T,int>)Myclass(T); // Conditionally
+
+	//explicit(true) Myclass(T); // C++20. Constructor is explicit
+	
+	//explicit(false) Myclass(T); // C++20. Constructor is not explicit
+	
+	//explicit(std::is_convertible_v<T,int>)Myclass(T); // Conditionally
 };
