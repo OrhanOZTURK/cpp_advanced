83aee867628ef57aa9eb7bf9f0254fa51f38419b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-01-03 10:47:59 +0300

    Update and rename ders_26/example7.cpp to ders_26/format/fm_11.cpp

--- ders_26/format/fm_11.cpp
+++ ders_26/format/fm_11.cpp
@@ -12,18 +12,18 @@ int main()
 
 	auto result = format_to_n(buffer, std::size(buffer) - 1, "{} isminin uzunlugu {}.", name, name.length());
 
-	std::cout << buffer << '\n'; //UB. Not null terminated byte stream.
+	cout << buffer << '\n'; //UB. Not null terminated byte stream.
 
-	std::cout << result.out << "\n"; // UB.
-	
-	std::cout << result.size << "\n"; // 33
-	
-	std::cout << result.out - buffer << "\n"; // 33
+	cout << result.out << "\n"; // UB.
+
+	cout << result.size << "\n"; // 33
+
+	cout << result.out - buffer << "\n"; // 33
 
 	//---------------------------------------------------------------------------------------------------------- -
 
-	*result.out = '\0'; 
+	*result.out = '\0';
 	// buffer[result.size] = '\0';
 
-	std::cout << "|" << buffer << "|"; //valid
+	cout << "|" << buffer << "|"; //valid
 }
