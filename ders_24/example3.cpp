2d682fc1ad0378e9fc44de4773393c34a02ebe5c
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-04 18:33:55 +0300

    Create example3.cpp

---
+++ ders_24/example3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <compare>
+
+int main()
+{
+	using namespace std;
+	
+	cout << boolalpha;
+
+	cout << (10 <=> 5 > 0) << '\n';		//true
+	cout << (10 <=> 5 < 0) << '\n';		//false
+	cout << (10 <=> 5 <= 0) << '\n';	//false
+	cout << (10 <=> 5 >= 0) << '\n';	//true
+	cout << (10 <=> 5 == 0) << '\n';	//false
+	cout << (10 <=> 5 != 0) << '\n';	//true
+}
