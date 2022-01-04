8cdaa98e8d8b67ee68c768aa95a97c0f515e951a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-01-04 09:49:11 +0300

    Update and rename ders_27/example13.cpp to ders_27/cpp20/14.cpp

--- ders_27/cpp20/14.cpp
+++ ders_27/cpp20/14.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-#include <vector>
 
 template<typename T>
 T::Neco func(); // Invalid at C++17. Valid at C++20
@@ -24,7 +22,7 @@ struct Nec {
 	using Ptr = PointerTrait<T>::Ptr; //Invalid at C++17.Valid in C++20.
 
 	T::Neco foo(T::Ali x) //C++20. Valid
-	{						
+	{
 
 	}
 };
