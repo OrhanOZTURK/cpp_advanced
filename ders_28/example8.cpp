4ad0bc0a618468c0389f24b596b3c8e49d6d3021
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-06 16:57:00 +0300

    Create example8.cpp

---
+++ ders_28/example8.cpp
@@ -0,0 +1,16 @@
+#include <concepts>
+#include <iostream>
+
+template <typename T>
+concept nec = requires(T x)
+{
+    std::cout << x;
+    std::cout >> x;
+};
+
+struct A{};
+
+int main()
+{
+    static_assert(nec<A>); //Syntax error
+}
