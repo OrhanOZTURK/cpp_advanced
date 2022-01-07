0b53633efec134de126c9f6ea42a9994efb844f0
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-01-07 10:07:42 +0300

    Update example6.cpp

--- ders_28/example6.cpp
+++ ders_28/example6.cpp
@@ -1,18 +1,18 @@
-#include <concepts>
-#include <iostream>
 #include <memory>
+#include <optional>
+
 
 template <typename T>
 concept nec = requires(T p)
 {
-    p == nullptr; // UNEVALUATED EXPRESSION.IFADENIN KENDİSİ GEÇERLİ OLMALI.TRUE FALSE OLMASI ÖNEMSİZ
+    p == nullptr; // unevaluated expression.ifadenın kendisi geçerli olmalı.true false olması önemsiz
 };
 
 int main() {
 
-    static_assert(nec<int *>); // GEÇERLİ
-    static_assert(nec<std::unique_ptr<int>>);  //GEÇERLİ
+    static_assert(nec<int*>); // valid
+    static_assert(nec<std::unique_ptr<int>>);  //valid
 
-    static_assert(nec<std::optional<int>>);  //GEÇERSİZ
-    static_assert(nec<std::optional<int *>>);  // GEÇERSİZ
+    static_assert(nec<std::optional<int>>);  //invalid
+    static_assert(nec<std::optional<int*>>);  // valid
 }
