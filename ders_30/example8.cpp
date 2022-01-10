7c0791100a78889d35e5a951b0d2f0a0f96318d2
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-10 17:19:23 +0300

    Create example8.cpp

---
+++ ders_30/example8.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+struct Employee
+{
+	std::string name;
+	int id;
+};
+
+struct Payslip
+{
+	std::string pay_info;
+	int id;
+};
+
+using namespace std;
+
+int main()
+{
+	vector<Employee> evec(100);
+	vector<Payslip> pvec(100);
+
+	ranges::sort(evec, [](const Employee& e1, const Employee& e2) { return e1.id < e2.id; });
+
+	ranges::sort(pvec, [](const Payslip& e1, const Payslip& e2) { return e1.id < e2.id; });
+
+	auto b = ranges::equal(evec, pvec, [](const Employee& e, const Payslip& p) { return e.id == p.id; });
+
+}
