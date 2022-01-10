0fe4b7f20062f7b7034b492502a18c61833d7db3
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-10 17:06:30 +0300

    Create example4.cpp

---
+++ ders_30/example4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	vector ivec{ 12, 5, 218, 9, 3, 24, -1, -9, -41, 6 };
+
+	ranges::sort(ivec); 
+
+	for (auto i : ivec)
+		std::cout << i << ' ';
+}
