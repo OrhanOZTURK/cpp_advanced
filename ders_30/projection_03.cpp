9d1bd6fc51a7fa5ad2bcae17658404a86e75ce56
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-01-13 00:35:00 +0300

    Update and rename example9.cpp to projection_03.cpp

--- ders_30/projection_03.cpp
+++ ders_30/projection_03.cpp
@@ -14,15 +14,18 @@ struct Payslip
 	int id;
 };
 
-using namespace std;
+
 
 int main()
 {
+	using namespace std;
+
 	vector<Employee> evec(100);
 	vector<Payslip> pvec(100);
 
 	ranges::sort(evec, ranges::less{}, [](const Employee& e) { return e.id; }); 
 	ranges::sort(pvec, ranges::less{}, [](const Payslip& e) { return e.id; });
 
-	auto b = ranges::equal(evec, pvec, std::ranges::equal_to{}, [](const Employee& e) { return e.id; }, [](const Payslip& p) { return p.id; });
+	auto b = ranges::equal(evec, pvec, ranges::equal_to{}, 
+			       [](const Employee& e) { return e.id; }, [](const Payslip& p) { return p.id; });
 }
