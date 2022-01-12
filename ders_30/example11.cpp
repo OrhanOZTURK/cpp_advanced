cca971f7ef3063cabf415df01d774a48333b075f
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-13 00:37:55 +0300

    Create example11.cpp

---
+++ ders_30/example11.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<ranges>
+#include<compare>
+#include<vector>
+#include<algorithm>
+#include<format>
+#include"nutility.h"
+
+
+struct Employee {
+	Employee() = default;
+	Employee(std::string name, std::string surname, int id) : m_name{ std::move(name) }, m_surname{ std::move(surname) } { }
+
+	auto operator<=> (const Employee&)const = default;
+
+	std::string m_name;  // Buradaki bildirimler ne sırada ise spaceship o sırada karşılaştırma kodu üretir
+	std::string m_surname;	// surname üstte olsaydı surname e göre sıralanacaktı
+	int mid{};
+};
+
+int main()
+{
+	using namespace std;
+	vector<Employee> evec;
+	for (int i = 0; i < 1000; ++i)
+	{
+		evec.emplace_back(rname(), rfname(), Irand{ 0,100000 }());
+	}
+	
+	sort(evec.begin(), evec.end()); //valid
+	
+	ranges::sort(evec, {}, &Employee::m_name); // Valid
+	
+	ranges::sort(evec, {}, [](const Employee& e) { return e.m_name + e.m_surname; }); // valid
+
+}
