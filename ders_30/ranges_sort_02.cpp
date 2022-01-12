5454f477e1bda3fc026850a0a527e4df0c291063
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-01-13 00:28:39 +0300

    Update and rename example5.cpp to ranges_sort_02.cpp

--- ders_30/ranges_sort_02.cpp
+++ ders_30/ranges_sort_02.cpp
@@ -2,13 +2,11 @@
 #include <vector>
 #include <algorithm>
 
-using namespace std;
-
 int main()
 {
 	vector ivec{ 12, 5, 218, 9, 3, 24, -1, -9, -41, 6 };
 
-	ranges::sort(ivec, {}, [](int x) { return std::abs(x); });
+	std::ranges::sort(ivec, {}, [](int x) { return std::abs(x); });
 
 	for (auto i : ivec)
 		std::cout << i << ' '; 	// -1 3 5 6 9 -9 12 24 -41 218
