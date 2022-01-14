99b4c1b31367b703a0d3761eea675bfd39cf940e
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-15 00:04:20 +0300

    Update example2.cpp

--- ders_31/example2.cpp
+++ ders_31/example2.cpp
@@ -75,3 +75,26 @@ Init Accumulate6(Iter beg, SenType end, Init init = Init{}, Op op = {})
 	}
 	return init;
 }
+
+
+----------------------------------------------------------------------------------------------------
+	
+	
+template<std::input_iterator Iter, std::sentinel_for<Iter> SenType, typename Init = std::iter_value_t<Iter>, typename Op = std::plus<>, typename Proj = std::identity>
+Init Accumulate7(Iter beg, SenType end, Init init = Init{}, Op op = {}, Proj proj = {})
+{
+	while (beg != end)
+	{
+		init = std::invoke(op, std::move(init), std::invoke(proj, *beg));
+		++beg;
+	}
+	return init;
+}
+
+----------------------------------------------------------------------------------------------------
+
+template<std::ranges::input_range R, class Init = std::ranges::range_value_t<R>, typename Op = std::plus<>, typename Proj = std::identity>
+Init Accumulate(R &&r, Init init = Init{}, Op op = {}, Proj proj = {})
+{
+	return Accumulate(std::ranges::begin(r), std::ranges::end(r),std::move(init), std::move(op), std::move(proj)); 
+}	
