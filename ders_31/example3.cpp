c223b26a60c055dbf832f695e9cb2297c201a7cf
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-15 00:07:13 +0300

    Create example3.cpp

---
+++ ders_31/example3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <ranges>
+#include <vector>
+#include <functional>
+#include "nutility.h"
+
+
+int main()
+{
+	using namespace std;
+
+	vector ivec{ 1,2,3,4,5 };
+
+	ranges::subrange sr1{ ivec }; // Ctad 
+
+	cout << "sizeof(sr1) = " << sizeof(sr1) << '\n'; // 8
+
+	sr1.front(); //valid
+	sr1.back(); //valid
+
+	ranges::subrange sr2{ ivec.begin(), ivec.end() };
+
+	for (const auto i : sr2)
+		std::cout << i << ' '; //1,2,3,4,5 
+}
