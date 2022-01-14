9d091efda6e9af7b8db2e6b43f038b9b3daf52d4
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-15 00:25:16 +0300

    Create example4.cpp

---
+++ ders_31/example4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <ranges>
+#include <vector>
+#include "nutility.h"
+
+int main()
+{
+	using namespace std;
+
+	vector ivec{ 1,2,3,4,5,15,25,40,60 };
+
+	//ranges::filter_view vw{ ivec, [](int x) { return x % 5 == 0; } }; //valid
+	//auto vw = views::filter(ivec, [](int x) { return x % 5 == 0; }); //valid
+	auto vw = ivec | views::filter([](int x) { return x % 5 == 0; }); //valid
+	
+	for (auto i : vw)
+	{
+		std::cout << i << " ";
+	}
+}
