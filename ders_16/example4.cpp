5da10cf1815f99a0706855d160a2f564efabc1c0
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-19 13:45:52 +0300

    Create example4.cpp

---
+++ ders_16/example4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+template <typename ...TS>
+auto leftfunc(TS ...args)
+{
+    return (0 + ... + args);
+}
+
+int main()
+{
+    std::cout << std::boolalpha;
+
+    std::cout << leftfunc(); // Valid code
+}
