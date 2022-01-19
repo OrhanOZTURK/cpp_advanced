217d50583a41c1198e26628f14538b25bf884da2
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-19 13:23:40 +0300

    Create example2.cpp

---
+++ ders_16/example2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+template <typename ...TS>
+auto leftfunc(TS ...args)
+{
+    return (... || args);
+}
+
+int main()
+{
+    std::cout << std::boolalpha;
+    std::cout << leftfunc(); // False
+}
