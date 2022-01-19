c6c1a2873f1c3412d5122dcb9789524a419f2248
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-19 14:41:09 +0300

    Create example18.cpp

---
+++ ders_16/example18.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+struct X {
+    X(int i)
+    {
+        std::cout << "X(int i) i = " << i << '\n';
+    }
+};
+
+struct Y {
+    Y(int i)
+    {
+        std::cout << "Y(int i) i = " << i << '\n';
+    }
+};
+
+struct Z {
+    Z(int i)
+    {
+        std::cout << "Z(int i) i = " << i << '\n';
+    }
+};
+
+template <typename ...Types>
+class A : public Types ... 
+{
+public:
+    A() : Types{ 0 }...
+    {
+    }
+};
+
+int main()
+{
+    A<X, Y, Z> ax;
+}
+
+OUTPUT
+------
+
+ÇIKTI
+-----
+X(int i) i = 0
+Y(int i) i = 0
+Z(int i) i = 0
