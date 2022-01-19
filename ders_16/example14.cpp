5cbd120de366aa2ac5412086ef3e522f1142e5f1
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-19 14:29:00 +0300

    Create example14.cpp

---
+++ ders_16/example14.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+bool pred(int i)
+{
+    return i % 2 == 0;
+}
+template <typename ...Ts>
+std::size_t count(Ts ...ts)
+{
+    return (std::size_t(0) + ... + (pred(ts) ? 1 : 0));
+}
+int main()
+{
+    std::cout << count(4,1,6,3,8,10); // 4
+    std::cout << count(4,1,6,3,8,10,1,3,8); // 5
+}
