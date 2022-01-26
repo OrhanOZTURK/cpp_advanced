d29d3d691cbf6ae47563820245c94d909156ae03
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-01-26 21:10:47 +0300

    Update crtp_singleton.cpp

--- ders_36/crtp_singleton.cpp
+++ ders_36/crtp_singleton.cpp
@@ -1,19 +1,20 @@
 #pragma once
+
 #include <memory>
 #include <mutex>
 
 template<typename Derived>
 class Singleton
 {
+private:
     static std::unique_ptr<Derived> m_instance;
     static std::once_flag           m_once;
 protected:
     Singleton() = default;
 public:
-    ~Singleton()
-    {
-        //...
-    }
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+    ~Singleton() = default;
     static Derived& get_instance()
     {
         std::call_once
@@ -30,3 +31,23 @@ std::unique_ptr<Derived>  Singleton<Derived>::m_instance;
 
 template<typename Derived>
 std::once_flag   Singleton<Derived>::m_once;
+
+#include <iostream>
+
+
+class Myclass : public Singleton<Myclass> {
+public:
+    void func()
+    {
+        std::cout << "Myclass::func()\n";
+    }
+    //...
+};
+
+int main()
+{
+    Myclass& s{ Myclass::get_instance() };
+    s.func();
+
+    // auto m = s; //gecersiz
+}
