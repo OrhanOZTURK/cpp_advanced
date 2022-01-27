5686f8690a11d74d6922996617f7db0ba0283827
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-01-27 14:19:48 +0300

    Create example10.cpp

---
+++ ders_32/example10.cpp
@@ -0,0 +1,43 @@
+#include<map>
+#include<ranges>
+#include<iostream>
+#include"nutility.h"
+#include<string>
+#include<utility>
+
+int main()
+{
+    using namespace std;
+
+    map<string,int> simap;
+    rfill(simap,20,[]{ return pair{rname() + ' ' + rfname(), Irand{0,10'000}() };} );
+
+    print(simap,"\n");
+
+    auto v1 = simap  | views::filter([](const auto &s){ return s.first.length() > 12; })
+                     | views::take(3)
+                     | views::keys;
+
+    for(const auto &elem : v1)
+        std::cout << elem << '\n';
+
+    std::cout << "---------------------------------------------------\n";
+
+    auto v2 = simap | views::filter([](const auto &s){ return s.first.length() > 12; })
+                    | views::take(3)
+                    | views::values;
+
+    for(const auto &elem : v2) {
+        std::cout << elem << '\n';
+    }
+
+    std::cout << "---------------------------------------------------\n";
+
+    auto v3 = simap | views::filter([](const auto &s){ return s.first.length() > 12; })
+                    | views::take(3)
+                    | views::elements<0>;
+
+                 for(const auto &elem : v3)
+    std::cout << elem << '\n';
+}
+
