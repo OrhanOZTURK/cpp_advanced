1a062bbac982dc9eb0472b3b4cb2b9a15d7a4d90
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-02-01 09:33:46 +0300

    Update example6.cpp

--- ders_32/example6.cpp
+++ ders_32/example6.cpp
@@ -1,12 +1,15 @@
-#include <iostream>
+//umut kotankıran tarafından aktarıldı
+
+#include <utility>  //std::declval
 
 class Myclass{
 public:
     double func();
     Myclass() = delete;
 };
+
 int main()
 {
-        decltype(std::declval<Myclass>().func()) x{}; // valid
+    decltype(std::declval<Myclass>().func()) x{}; // valid
 }
-//umut kotankıran tarafından aktarıldı
+
