f7a027eedd54770292e3bd3b7548a388a7933c3f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-02-01 09:25:06 +0300

    Update and rename example1.cpp to non_type_template_param.cpp

--- ders_32/example1.cpp
+++
@@ -1,9 +0,0 @@
-template<auto n> // nontype parameter. 
-class Myclass{
-};
-
-int main()
-{
-	Myclass<5.5> x; 
-}
-//umut kotankıran tarafından aktarıldı
---
+++ ders_32/non_type_template_param.cpp
@@ -0,0 +1,12 @@
+//Umut Kotankıran tarafından aktarıldı
+
+template<auto n> // nontype parameter. 
+class Myclass {
+	//...
+};
+
+int main()
+{
+	Myclass<5.5> x; //C++20
+}
+
