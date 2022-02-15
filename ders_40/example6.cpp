670815e1368b104fd4ac02bc66f49433f36c7c92
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-02-15 12:32:58 +0300

    Create example6.cpp

---
+++ ders_40/example6.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include <stdexcept>
+
+void foo()
+{
+    std::cout << "foo cagrilir\n";
+    throw;
+}
+void func()
+{
+    try{
+        throw std::out_of_range{"range hatasi"};
+    }
+    catch(const std::exception& ex)
+    {
+        std::cout << "hata func icinde yakalandi : " << ex.what() << '\n';
+        foo();
+    }
+}
+int main()
+{
+    try
+    {
+        func();
+    }
+    catch(const std::out_of_range&)
+    {
+        std::cout << "hata yakalandi (std::out_of_range)\n";
+    }
+    catch(const std::exception&)
+    {
+        std::cout << "hata yakalandi (std::exception)\n";
+    }
+}
