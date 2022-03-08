25c5f08e1538d1893811159d612d85b64614090c
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-03-08 19:38:58 +0300

    Create example3.cpp

---
+++ ders_42/example3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <tuple>
+
+int main()
+{
+	using namespace std;
+
+	int x = 10, y = 20, z = 30, t = 40;
+	cout << "x = " << x << '\n';
+	cout << "y = " << y << '\n';
+	cout << "z = " << z << '\n';
+	cout << "t = " << t << '\n';
+	std::cout << "------\n";
+	tie(x, y, z, t) = tuple{ y,z,t,x }; 
+	cout << "x = " << x << '\n';
+	cout << "y = " << y << '\n';
+	cout << "z = " << z << '\n';
+	cout << "t = " << t << '\n';
+}
+
+OUTPUT
+------
+x = 10
+y = 20
+z = 30
+t = 40
+------
+x = 20
+y = 30
+z = 40
+t = 10
