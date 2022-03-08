464fdb77e8e355acb76d091a04689be8563e52bb
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-03-08 19:36:54 +0300

    Update example2.cpp

--- ders_42/example2.cpp
+++ ders_42/example2.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <tuple>
 
+int main()
+{
+	int x = 23, y = 34;
+	std::cout << "x = " << x << " y = " << y << '\n'; 
+	
+	std::tie(x, y) = std::pair{ y,x };
+	std::cout << "x = " << x << " y = " << y << '\n'; 
+}
+
+OUTPUT
+------
+x = 23 y = 34
+x = 34 y = 23
