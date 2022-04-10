ce90b1888a216136dd4d706546bad4c3e7f02369
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-04-10 21:19:23 +0300

    Update JThread.cpp

--- ders_48/JThread.cpp
+++ ders_48/JThread.cpp
@@ -1,1 +1,51 @@
+class JThread
+{
+public:
+	JThread()noexcept = default;
 
+	template<typename Func, typename ...Args>
+	explicit JThread(Func&& f, Args &&...args) : m_t(std::forward<Func>(f), std::forward<Args>(args)...)
+	{
+		
+	}
+
+	explicit JThread(std::thread t)noexcept : m_t(std::move(t)) { }
+	
+	JThread(JThread&& other)noexcept : m_t(std::move(other.m_t)) { }
+
+	bool joinable()const noexcept
+	{
+		return m_t.joinable();
+	}
+
+	~JThread()
+	{
+		if(joinable())
+		{
+			join();
+		}
+	}
+
+	void join()
+	{
+		m_t.join();
+	}
+
+	void swap(JThread&other)noexcept
+	{
+		m_t.swap(other.m_t);
+	}
+
+	JThread& operator=(JThread&& other)
+	{
+		if(joinable())
+			join();
+
+		m_t = std::move(other.m_t);
+
+		return *this;
+	}
+
+private:
+	std::thread m_t;
+};
