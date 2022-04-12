4f2d883b52d03e4a2b66251cad1db12a7e0644e4
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-04-12 09:13:39 +0300

    Update 06.cpp

--- ders_27/cpp20/06.cpp
+++ ders_27/cpp20/06.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 struct Nec {
 	const int& r;
 };
@@ -8,9 +6,10 @@ int foo()
 {
 	return 1;
 }
+
 int main()
 {
 	Nec n{ foo() }; //Valid. life extension
 
-	Nec m( foo() ); // Ignore most mexing parse. There isn't life extension
+	Nec m(foo()); // Ignore most vexing parse. There isn't life extension
 }
