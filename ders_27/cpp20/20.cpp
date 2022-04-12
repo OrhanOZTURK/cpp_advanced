d54c6924f983a6bb7486364cbac9374c8caa694d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-04-12 09:29:15 +0300

    Update 20.cpp

--- ders_27/cpp20/20.cpp
+++ ders_27/cpp20/20.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	int x = -1;
-	if ((x >> 1) == -1)	//C++17 Implementation defined. C++20 guranteeed to be true
+	if ((x >> 1) == -1)	//C++17 Implementation defined. C++20 guaranteeed to be true
 	{
 		std::cout << "dogru\n"; 
 	}
