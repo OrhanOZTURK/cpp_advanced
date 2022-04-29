ebfa803599969640b6af1c0653fdf433b727b89d
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2022-04-29 16:10:21 +0300

    Create example3.cpp

---
+++ ders_49/example3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <future>
+
+void func()
+{
+	std::cout << "func cagrildi\n";
+	if (1)
+	{
+		throw std::runtime_error{ "error from func\n" };
+	}
+}
+
+void my_terminate()
+{
+	std::cout << "My terminate called\n";
+	abort();
+}
+
+int main()
+{
+	set_terminate(my_terminate);
+
+	try {
+		std::thread t{ func };  // Burada exception gönderiliyor ama yakalanmıyor.
+								// Doğrudan std::terminate çağrılıyor.
+		t.join();
+	}
+	catch (const std::exception& ex)
+	{
+		std::cout << ex.what() << '\n';
+	}
+}
