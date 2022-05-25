787159706563c56837f68193ca98b8fa80be63a5
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-05-25 18:27:06 +0300

    Update spaceship_02.cpp

--- ders_24/spaceship_02.cpp
+++ ders_24/spaceship_02.cpp
@@ -8,11 +8,11 @@ int main()
 
 	std::cout << std::boolalpha;
 	std::cout << (std::strong_ordering::greater > 0) << '\n';	  //TRUE
-	std::cout << (std::partial_ordering::unordered > 0) << '\n';; //FALSE
-	std::cout << (std::partial_ordering::unordered < 0) << '\n';; //FALSE 
+	std::cout << (std::partial_ordering::unordered > 0) << '\n'; //FALSE
+	std::cout << (std::partial_ordering::unordered < 0) << '\n'; //FALSE 
 
-	std::cout << (std::strong_ordering::less > 0) << '\n';;	  //FALSE 
-	std::cout << (std::strong_ordering::less == 0) << '\n';;  //FALSE
-	std::cout << (std::strong_ordering::less != 0) << '\n';;  //TRUE 
-	std::cout << (std::strong_ordering::less >= 0) << '\n';;  //FALSE 
+	std::cout << (std::strong_ordering::less > 0) << '\n';	  //FALSE 
+	std::cout << (std::strong_ordering::less == 0) << '\n';  //FALSE
+	std::cout << (std::strong_ordering::less != 0) << '\n';  //TRUE 
+	std::cout << (std::strong_ordering::less >= 0) << '\n';  //FALSE 
 }
